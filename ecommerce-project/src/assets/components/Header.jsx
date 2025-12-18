c20b786b103f75ed9db60067b70f2b89fc593b11
ssangam1024 <ssangam1024@gmail.com>
2025-12-17 22:25:28 -0700

    Move tracking page into react

--- ecommerce-project/src/assets/components/Header.jsx
+++ ecommerce-project/src/assets/components/Header.jsx
@@ -6,9 +6,9 @@ export function Header(){
             <div className="left-section">
             <NavLink to="/" className="header-link">
                 <img className="logo"
-                src="images/logo-white.png" />
+                src="../images/logo-white.png" />
                 <img className="mobile-logo"
-                src="images/mobile-logo-white.png" />
+                src="../images/mobile-logo-white.png" />
             </NavLink>
             </div>
     
