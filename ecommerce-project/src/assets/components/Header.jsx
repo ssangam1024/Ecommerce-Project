18aae5a77ba6d489ad9d4aa3b9aab34a8de9a4e1
ssangam1024 <ssangam1024@gmail.com>
2025-12-27 22:18:14 -0700

    get cart from the backend and display quantity

--- ecommerce-project/src/assets/components/Header.jsx
+++ ecommerce-project/src/assets/components/Header.jsx
@@ -4,7 +4,12 @@ import SearchIcon from '../images/icons/search-icon.png';
 import LogoWhite from '../images/logo-white.png';
 import MobileLogoWhite from '../images/mobile-logo-white.png';
 import './Header.css'
-export function Header(){
+export function Header({ cart }){
+    let totalQuantity = 0;
+
+    cart.forEach( ( cartItem )=> {
+        totalQuantity += cartItem.quantity;
+    });
     return(
         <div className="header">
             <div className="left-section">
@@ -32,7 +37,7 @@ export function Header(){
     
             <NavLink className="cart-link header-link" to="/checkout">
                 <img className="cart-icon" src={CartIcon} />
-                <div className="cart-quantity">3</div>
+                <div className="cart-quantity"> {totalQuantity} </div>
                 <div className="cart-text">Cart</div>
             </NavLink>
             </div>
