db6d16b79e80a944449c91db564e0e62f0cc2172
ssangam1024 <ssangam1024@gmail.com>
2025-12-30 22:52:56 -0700

    convert orders to async await

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -13,10 +13,11 @@ export function OrdersPage({ cart }) {
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
-        axios.get('/api/orders?expand=products')
-            .then((response) => {
-                setOrders(response.data);
-            });
+        const fectOrdersData = async() => {
+            const response = await axios.get('/api/orders?expand=products');
+            setOrders(response.data);
+        }
+        fectOrdersData();
     }, []);
 
     return (
